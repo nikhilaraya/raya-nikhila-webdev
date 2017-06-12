f1b5956f5f9494cb26349b79d940fcc06fb21f3b
nikhilaraya <raya.n@husky.neu.edu>
2017-06-12 16:33:43 -0400

    change connection string

--- assignment/app.js
+++ assignment/app.js
@@ -1,14 +1,19 @@
 var mongoose = require('mongoose');
 var connectionString = 'mongodb://127.0.0.1:27017/webdev_summer1_2017';
 
-if(process.env.MLAB_USERNAME) {
+/**if(process.env.MLAB_USERNAME) {
     connectionString = process.env.MLAB_USERNAME + ":" +
         process.env.MLAB_PASSWORD + "@" +
         process.env.MLAB_HOST + ':' +
         process.env.MLAB_PORT + '/' +
         process.env.MLAB_APP_NAME;
+}**/
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds139761.mlab.com:39761/heroku_97xkgvvq'; // user yours
 }
-
 mongoose.connect(connectionString);
 mongoose.Promise = require('q').Promise;
 
