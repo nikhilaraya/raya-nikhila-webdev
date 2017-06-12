f3beda3d4ff19eb2fb0c1c849172631fbb1e5d21
nikhilaraya <raya.n@husky.neu.edu>
2017-06-12 16:08:15 -0400

    connection string

--- assignment/app.js
+++ assignment/app.js
@@ -1,5 +1,15 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/webdev_summer1_2017');
+var connectionString = 'mongodb://127.0.0.1:27017/webdev_summer1_2017';
+
+if(process.env.MLAB_USERNAME) {
+    connectionString = process.env.MLAB_USERNAME + ":" +
+        process.env.MLAB_PASSWORD + "@" +
+        process.env.MLAB_HOST + ':' +
+        process.env.MLAB_PORT + '/' +
+        process.env.MLAB_APP_NAME;
+}
+
+mongoose.connect(connectionString);
 mongoose.Promise = require('q').Promise;
 
 require('./services/user.service.server');
